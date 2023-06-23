de9140c5e5e7305b49ca263fb12028584fa398a7
Nicole Su <tingchis@usc.edu>
2023-06-23 11:14:31 -0700

    Add more files

---
+++ version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+env=$1
+service=$2
+
+for h in $(~/code/tools/gcp_tools/gdns/bin/gdns -o fqdn -e ${env} ${service})
+do
+    echo -n "${h} "
+    ssh -q -o ConnectTimeout=10 ${h} "cat /opt/sift/${service}/current/REVISION"
+done |awk '{print $2" "$1}'
